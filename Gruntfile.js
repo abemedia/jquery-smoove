e913664db67fe5b995c793d50f39bcf8192ddfb1
koding user <adam@abemedia.co.uk>
2014-12-16 01:40:01 +0000

    Fix grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,13 +41,7 @@ module.exports = function (grunt) {
             }
         },
         qunit: {
-            all: {
-                options: {
-                    urls: ['1.7.0', '1.8.0', '1.9.0', '1.10.0', '2.0.0b1'].map(function (version) {
-                        return 'http://0.0.0.0:<%= connect.server.options.port %>/test/smoove.html?jquery=' + version;
-                    })
-                }
-            }
+          files: ['test/**/*.html']
         },
         jshint: {
             options: {
